5d44e467cc059a939dcce6d592afabafb9065662
Alessandro Francesconi <alessandrofrancesconi@live.it>
2022-11-06 12:55:40 +0100

    Added W25Q128JV to possible_devices

--- src/Adafruit_SPIFlashBase.cpp
+++ src/Adafruit_SPIFlashBase.cpp
@@ -99,7 +99,7 @@ static const SPIFlash_Device_t possible_devices[] = {
     MB85RS64V, MB85RS1MT, MB85RS2MTA, MB85RS4MT,
 
     // Other common flash devices
-    W25Q16JV_IQ, W25Q32JV_IQ, AT25SF041, AT25DF081A};
+    W25Q16JV_IQ, W25Q32JV_IQ, AT25SF041, AT25DF081A, W25Q128JV_PM, W25Q128JV_SQ};
 
 /// Flash device list count
 enum {
